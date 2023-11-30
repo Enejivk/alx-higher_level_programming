2f9eee7e6a13af5bb74a9dfc8629243ff8b9fecf
enejivk <enejivic@gmail.com>
2023-11-30 14:49:45 +0100

    Uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for lower_case in str:
+        if ord(lower_case) >= 97 and ord(lower_case) <= 123:
+            lower_case = chr(ord(lower_case) -32)
+        print("{}".format(lower_case), end="")
+    print("") 
+        
