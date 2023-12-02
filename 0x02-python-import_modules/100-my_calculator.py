89058f5eae61a3384ed05d981df05c35aeff6eef
enejivk <enejivic@gmail.com>
2023-12-02 04:06:32 +0100

    Command line argurment

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    from sys import argv, exit
     from calculator_1 import add, sub, mul, div
-    lent = len(sys.argv)
-    if lent < 3:
+    lent = len(argv)
+    if lent < 2:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    n1 = int(sys.argv[1])
-    operator = sys.argv[2]
-    n2 = int(sys.argv[3])
+        exit(1)
+    n1 = int(argv[1])
+    operator = argv[2]
+    n2 = int(argv[3])
     match operator:
         case "+":
-            print("{} + {} = {}".format(n1, n2, calculator_1.add(n1, n2)))
+            print("{} + {} = {}".format(n1, n2, add(n1, n2)))
         case "-":
-            print("{} - {} = {}".format(n1, n2, calculator_1.sub(n1, n2)))
+            print("{} - {} = {}".format(n1, n2, sub(n1, n2)))
         case "/":
-            print("{} / {} = {}".format(n1, n2, calculator_1.div(n1, n2)))
+            print("{} / {} = {}".format(n1, n2, div(n1, n2)))
         case "*":
-            print("{} * {} = {}".format(n1, n2, calculator_1.mul(n1, n2)))
+            print("{} * {} = {}".format(n1, n2, mul(n1, n2)))
         case _:
             print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+            exit(1)
