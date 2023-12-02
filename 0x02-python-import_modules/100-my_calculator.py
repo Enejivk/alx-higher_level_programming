69130c52e76a297b4fee3a4cd0632634ea50c9d8
enejivk <enejivic@gmail.com>
2023-12-02 04:15:11 +0100

    Command line argurment

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,22 +2,33 @@
 if __name__ == "__main__":
     from sys import argv, exit
     from calculator_1 import add, sub, mul, div
-    lent = len(argv)
-    if lent < 2:
+    
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    n1 = int(argv[1])
-    operator = argv[2]
-    n2 = int(argv[3])
-    match operator:
-        case "+":
-            print("{} + {} = {}".format(n1, n2, add(n1, n2)))
-        case "-":
-            print("{} - {} = {}".format(n1, n2, sub(n1, n2)))
-        case "/":
-            print("{} / {} = {}".format(n1, n2, div(n1, n2)))
-        case "*":
-            print("{} * {} = {}".format(n1, n2, mul(n1, n2)))
-        case _:
-            print("Unknown operator. Available operators: +, -, * and /")
+    
+    try:
+        n1 = int(argv[1])
+        operator = argv[2]
+        n2 = int(argv[3])
+    except ValueError:
+        print("Error: Please provide valid integer numbers.")
+        exit(1)
+
+    result = None
+    if operator == "+":
+        result = add(n1, n2)
+    elif operator == "-":
+        result = sub(n1, n2)
+    elif operator == "*":
+        result = mul(n1, n2)
+    elif operator == "/":
+        if n2 == 0:
+            print("Error: Division by zero is not allowed.")
             exit(1)
+        result = div(n1, n2)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(n1, operator, n2, result))
