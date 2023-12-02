1c7fddae70303ac76ce96a57e5cfe34c5bbd2b3e
enejivk <enejivic@gmail.com>
2023-12-02 03:44:38 +0100

    Add using command line argument

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    import calculator_1
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
     lent = len(sys.argv)
     if lent < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
