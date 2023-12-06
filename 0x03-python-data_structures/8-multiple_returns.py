4aa1c24bd5589f78388eeffb2b940830f64d8692
enejivk <enejivic@gmail.com>
2023-12-06 22:12:00 +0100

    Return length and first character

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    sent_length = len(sentence)
+    
+    if not sentence:
+        return sent_length, None
+    else:
+        return sent_length, sentence[0]
