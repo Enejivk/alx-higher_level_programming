fe9bdeda74e3a57dae25e332f381d6d81bae7268
enejivk <enejivic@gmail.com>
2023-12-07 06:12:01 +0100

    Return max without using max buiding function

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,5 +2,8 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    list_max = max(my_list)
-    return list_max
+    larget_number = my_list[0]
+    for i in range(1, len(my_list)):
+        if my_list[i] > larget_number:
+            larget_number = my_list[i]
+    return larget_number
