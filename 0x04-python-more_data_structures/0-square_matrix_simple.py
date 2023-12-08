94bf1871eca3f6ba1edfc940644a9c62f0810f59
enejivk <enejivic@gmail.com>
2023-12-08 13:42:16 +0100

    square a matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in range(len(matrix)):
+        row_list = []
+        for j in range(len(matrix[i])):
+            square = matrix[i][j] ** 2
+            row_list.append(square)
+        new_matrix.append(row_list)
+    return new_matrix
