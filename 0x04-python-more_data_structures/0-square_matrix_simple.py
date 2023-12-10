c3001568d2953a1715c0533263f85548993b2c7a
enejivk <enejivic@gmail.com>
2023-12-10 20:46:23 +0100

    Square matrix using map

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
-        square = list(map(lambda x: x **2, row))
+        square = list(map(lambda x: x ** 2, row))
         new_matrix.append(square)
     return new_matrix
