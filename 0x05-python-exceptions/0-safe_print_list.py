b28d17f378a561c6e5e2a2499982c2999b6c73c6
enejivk <enejivic@gmail.com>
2023-12-18 16:28:28 +0100

    print and nth number from a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        count = 0
-        for i in range(x):
+        i = 0
+        while i < x:
             print("{}".format(my_list[i]), end="")
-            count += 1
+            i=i+1
         print()
-        return count
+        return i
     except IndexError:
         print()
-        return count
+        return i
