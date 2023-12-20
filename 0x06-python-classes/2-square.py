d608e56d7d55e426b5f762fbda5b0208461f3271
enejivk <enejivic@gmail.com>
2023-12-20 03:14:39 +0100

    initialing of size in square class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class"""
+
+
+class Square:
+    """
+    Attribute
+    """
+    def __init__(self, size=0):
+        """
+        Initializing square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
