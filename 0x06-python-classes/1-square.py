029706980df3e94b99f23e3c6d491860c0572fc0
enejivk <enejivic@gmail.com>
2023-12-20 02:25:03 +0100

    square with an attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This class contain a size attribute"""
+
+
+class Square:
+    """This class contain an attribute size"""
+
+    def __init__(self, size):
+        """Initializing square
+        Args:
+            size (int): size of a square
+        """
+        self.__size = size
