5b54e0f82e5f1c48e088af258053bd2d50102b27
enejivk <enejivic@gmail.com>
2024-01-05 04:37:34 +0100

    parameter and area of a rectangl

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Creating a Rectangle class"""
+
+
+class Rectangle:
+    """Creating a private instance of width and height"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+    
+    def area(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return self.__height * self.__width
+        
+    def perimeter(self):
+        add_para = self.__height + self.__width
+        return 2 * add_para
