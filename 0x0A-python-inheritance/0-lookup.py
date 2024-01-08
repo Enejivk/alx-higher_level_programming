7f7f0b92703a8bcf89ed1b12a6c6e27ebefb9105
enejivk <enejivic@gmail.com>
2024-01-08 17:00:21 +0100

    All the available method

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""This function searches for object and print it out"""
+
+
 def lookup(obj):
     """This function returns the list of all attribute and method"""
+
     return dir(obj)
