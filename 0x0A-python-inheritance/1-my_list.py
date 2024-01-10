df940201d52a260e1d1a9ba36f3e79bc5f77ce4d
enejivk <enejivic@gmail.com>
2024-01-10 09:49:31 +0100

    print element in sorted form

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,7 @@
-#!/usr/bin/python3
-MyList = __import__('1-my_list').MyList
+#!/bin/usr/python3
 
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
+
+"""Creatin a class my"""
+class MyList(list):
+    def print_sorted(self):
+        print(sorted(self))
