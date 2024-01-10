d7ea648ec3d36315af235271243b29a1e8c1d28d
enejivk <enejivic@gmail.com>
2024-01-10 23:13:46 +0100

    Calculating Area

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Creating a class that raises an exception"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Creating a derived class of BaseGeometry
+
+    private instancess attributes:
+    - width
+    - height
+    """
+    def __init__(self, width, height):
+        """initializing rectangle instnaces
+
+        Args:
+            width: width of the rectangle
+            height: height of the Rectangle
+        """
+
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculating The area of a rectangle"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        """Magic function that print the rectagle"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
