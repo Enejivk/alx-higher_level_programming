d6ef8fef7b4bb840c189990137c1cbe8005db2f0
enejivk <enejivic@gmail.com>
2024-01-10 12:09:26 +0100

    Checking for an instance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pyyhon3
+"""Checking if it is an object or a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checking if it belongs to the same class"""
+    return isinstance(obj, a_class)
