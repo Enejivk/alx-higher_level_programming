564ae90c4e1b929436effe40cf8dcfde79a399e9
enejivk <enejivic@gmail.com>
2024-01-13 12:33:40 +0100

    Read the content of a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,7 @@ print the content of the file
 
 
 def read_file(filename=""):
+    """opening a file"""
     with open(filename, "r", encoding="utf-8") as file:
         readfile = file.read()
         print(readfile, end="")
