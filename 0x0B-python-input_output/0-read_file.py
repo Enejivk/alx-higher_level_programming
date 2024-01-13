024e86239f76a31147bea52efa328f3b24bc6a7f
enejivk <enejivic@gmail.com>
2024-01-13 12:26:23 +0100

    Read al

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,8 @@ This function opens a file
 Read the content of the file
 print the content of the file
 """
+
+
 def read_file(filename=""):
     with open(filename, encoding="utf-8") as file:
         readfile = file.read().strip()
