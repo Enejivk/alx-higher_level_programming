3e995851da709be588c94bc74d0ecfe034636cfb
enejivk <enejivic@gmail.com>
2024-01-13 13:12:33 +0100

    write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,15 +6,6 @@ return the numbers of character written
 
 
 def write_file(filename="", text=""):
-    """write_file: the funtion write to a file
-
-    Args:
-        filename: This contain the name of file to be written.
-        text: This contain all the text to be written to the file.
-
-    Returns:
-        It return the total character written
-    """
+    """write_file: the funtion write to a file"""
     with open(filename, "w", encoding="utf8") as file:
-        written = file.write(text)
-        return written
+        return file.write(text)
