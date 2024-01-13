7b80d88e8de4c4ef2e314f0de0c3b477c9e93bb5
enejivk <enejivic@gmail.com>
2024-01-13 13:43:24 +0100

    Json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """jason reprsentation of a dictionary"""
-
-
 import json
+
+
 def to_json_string(my_obj):
     """This funtion return a dictionary"""
     return json.dumps(my_obj, sort_keys="true")
