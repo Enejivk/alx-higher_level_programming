7453339c3d5733a93cc203206152ce5723bc0edd
enejivk <enejivic@gmail.com>
2024-01-14 20:04:06 +0100

    Return a specfic value from a dictionary

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,26 +1,25 @@
-# #!/usr/bin/python3
-# """
-# Student class module
-# """
+#!/usr/bin/python3
+"""
+Student class module
+"""
 
 
-# class Student:
-#     """Student class
-#     """
-#     def __init__(self, first_name, last_name, age):
-#         self.first_name = first_name
-#         self.last_name = last_name
-#         self.age = age
+class Student:
+    """Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
             
-#     def to_json(self, attrs=None):
-#         """It This funtion returns dictionary of all the attribute
-#         """
-#         attribute_dict = vars(self)
-#         if attrs == None:
-#             return attribute_dict
-
-#         new_attribute_dict = {}
-#         for key in attrs:
-#             if key in attribute_dict:
-#                new_attribute_dict[key] = attribute_dict[key]
-#         return new_attribute_dict
+    def to_json(self, attrs=None):
+        """It This funtion returns dictionary of all the attribute
+        """
+        attribute_dict = vars(self)
+        if attrs == None:
+            return attribute_dict
+        new_attribute_dict = {}
+        for key in attrs:
+            if key in attribute_dict:
+               new_attribute_dict[key] = attribute_dict[key]
+        return new_attribute_dict
