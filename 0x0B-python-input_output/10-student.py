77989289aa30bb04a1c82fb44e9867ce78538842
enejivk <enejivic@gmail.com>
2024-01-14 21:11:45 +0100

    some element in a dict

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-""" Module that defines the class Student
+"""
+Student class module
 """
 
 
 class Student:
-    """ Class to create student instances """
+    """Student class
+    """
 
     def __init__(self, first_name, last_name, age):
-        """ Special method to initialize """
+        """Initialize the conten of student module"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ Method that returns directory description """
-        obj = self.__dict__.copy()
+        """ It This funtion returns dictionary of all the attribute """
+        attribute_dict = vars(self)
         if type(attrs) is list:
 
-            for item in attrs:
-                if type(item) is not str:
-                    return obj
+            for attr in attrs:
+                if type(attr) is not str:
+                    return attribute_dict
 
-            d_list = {}
+            new_attribute_dict = {}
+            for key in attrs:
+                if key in attribute_dict:
+                    new_attribute_dict[key] = attribute_dict[key]
+            return new_attribute_dict
 
-            for iatr in range(len(attrs)):
-                for satr in obj:
-                    if attrs[iatr] == satr:
-                        d_list[satr] = obj[satr]
-            return d_list
-
-        return obj
+        return attribute_dict
