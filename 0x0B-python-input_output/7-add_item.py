4081b4d3b3895305dda392fa5c0aafb3545851ae
enejivk <enejivic@gmail.com>
2024-01-14 07:40:14 +0100

    Command line arguemnt to file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,13 +9,10 @@ if __name__ == "__main__":
         __import__('6-load_from_json_file').load_from_json_file
 
 
-mylist = []
-for i in range(1, len(sys.argv)):
-    mylist.append(sys.argv[i])
 try:
     my_list = load_from_json_file("add_item.json")
 except FileNotFoundError:
     my_list = []
-    """Extending the contennt of the initial list"""
-my_list.extend(mylist)
+
+my_list.extend(sys.argv[1:])
 save_to_json_file(my_list, "add_item.json")
