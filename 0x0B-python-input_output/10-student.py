e3d26043342ec3a48fd1b59c3d9939da826c1ee6
enejivk <enejivic@gmail.com>
2024-01-14 21:09:23 +0100

    some element in a dict

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -7,13 +7,15 @@ Student class module
 class Student:
     """Student class
     """
+
     def __init__(self, first_name, last_name, age):
+        """Initialize the conten of student module"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """It This funtion returns dictionary of all the attribute"""
+        """ It This funtion returns dictionary of all the attribute """
         attribute_dict = vars(self)
         if type(attrs) is list:
 
