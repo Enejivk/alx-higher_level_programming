e8785e516e68bd3b15c5df66b35e98f25ff90216
enejivk <enejivic@gmail.com>
2024-01-14 19:56:59 +0100

    Return a specfic value from a dictionary

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""
+Student class module
+"""
 
 
 class Student:
-    """Represent a student."""
-
+    """Student class
+    """
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
+            
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
+        """It This funtion returns dictionary of all the attribute"""
+        attribute_dict = vars(self)
+        if attrs == None:
+            return attribute_dict
 
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+        new_attribute_dict = {}
+        for key in attrs:
+            if key in attribute_dict:
+               new_attribute_dict[key] = attribute_dict[key]
+        return new_attribute_dict
