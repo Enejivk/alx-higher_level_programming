ef0878ac68031992d31836f841d12b51c912e4b5
enejivk <enejivic@gmail.com>
2024-01-14 21:06:29 +0100

    some element in a dict

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,17 +11,20 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-            
+
     def to_json(self, attrs=None):
-        """It This funtion returns dictionary of all the attribute
-        """
+        """It This funtion returns dictionary of all the attribute"""
         attribute_dict = vars(self)
-        if attrs == None:
-            return attribute_dict
+        if type(attrs) is list:
 
-        new_attribute_dict = {}
-        for key in attrs:
-            if key in attribute_dict:
-               new_attribute_dict[key] = attribute_dict[key]
+            for attr in attrs:
+                if type(attr) is not str:
+                    return attribute_dict
 
-        return new_attribute_dict
+            new_attribute_dict = {}
+            for key in attrs:
+                if key in attribute_dict:
+                    new_attribute_dict[key] = attribute_dict[key]
+            return new_attribute_dict
+
+        return attribute_dict
