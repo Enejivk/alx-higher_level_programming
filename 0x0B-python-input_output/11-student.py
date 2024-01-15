08fdbffe0338d480b8808db5157aaee82fa6c845
enejivk <enejivic@gmail.com>
2024-01-15 09:06:54 +0100

    replace all the element found in self

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -25,13 +25,13 @@ class Student:
             if key in attribute_dict:
                 newattribute_dict[key] = attribute_dict[key]
         return newattribute_dict
-    
+
     def reload_from_json(self, json):
         """Reload_from_json: It takes an attribute of self
         and convert it to the respective value stored in json.
         Arg
         self: This contain all the attribute of the object
-        json: This is contain the value to be replace with the value in 
+        json: This is contain the value to be replace with the value in
         self.
         """
         self.age = json['age']
