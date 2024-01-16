cf9147ce78b1fd47b239386ee7da1f1f4a08dd66
enejivk <enejivic@gmail.com>
2024-01-16 14:26:26 +0100

    print string rep

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,5 +88,4 @@ class Rectangle(Base):
 
     def __str__(self):
         """Print string represention of this object"""
-        return f'[Rectangle] ({self.id}) {self.x}/{self.y} -\
-            {self.width}/{self.height}'
+        return '[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.x, self.y, self.width, self.height)
