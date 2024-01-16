3e01c1cc96735009a72892aa525fcc24e9e5f83a
enejivk <enejivic@gmail.com>
2024-01-16 13:09:08 +0100

    testing

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,7 +27,21 @@ class Base:
 
 
 class Rectangle(Base):
+    """_summary_
+
+    Args:
+        Base (_type_): _description_
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """_summary_
+
+        Args:
+            width (_type_): _description_
+            height (_type_): _description_
+            x (int, optional): _description_. Defaults to 0.
+            y (int, optional): _description_. Defaults to 0.
+            id (_type_, optional): _description_. Defaults to None.
+        """
         super().__init__(id)
         self.width = width
         self.height = height
