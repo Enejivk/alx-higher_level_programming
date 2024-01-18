04402344e96ffbc8a5cb649da2eaa2437cdefbe9
enejivk <enejivic@gmail.com>
2024-01-18 10:55:25 +0100

    printing of values

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -34,3 +34,27 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         else:
             self.__size = value
+
+    def update(self, *args, **kwargs):
+        """printing the value of attribute
+        """
+
+        if len(args) != 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key in kwargs:
+                if key == "id":
+                    self.id = kwargs["id"]
+                if key == "size":
+                    self.size = kwargs["size"]
+                if key == "y":
+                    self.y = kwargs["y"]
+                if key == "x":
+                    self.x = kwargs["x"]
