bba5a70d1d64c27e787b3c57d3072c311bb7b447
enejivk <enejivic@gmail.com>
2024-01-18 10:18:59 +0100

    validating the value of a square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,10 +13,24 @@ class Square(Rectangle):
         """
         """
         super().__init__(size, size, x, y, id)
-        self.size = size
+        self.__size = size
 
     def __str__(self):
         """Return string rep. of attribute
         """
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+
+            raise ValueError("width must be > 0")
+        else:
+            self.__size = value
