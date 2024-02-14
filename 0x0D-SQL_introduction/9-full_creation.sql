06f79e289fcbb0052a24d336e8a7b2f15304a361
enejivk <enejivic@gmail.com>
2024-02-14 11:57:38 +0100

    Creating Table and a row at thesame time

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,12 +1,12 @@
 -- Creating a full table with values
 
-CREATE TABLE IF NOT EXISTS `testing2`(
+CREATE TABLE IF NOT EXISTS `second_table`(
     id INT,
     name VARCHAR(256),
     score INT
     );
 
-INSERT INTO `testing2` (id, name, score) 
+INSERT INTO `second_table` (id, name, score) 
 VALUES 
     ('1', 'John', '10'),
     ('2', 'Alex', '3'),
