17273646b832c7b10602bfac382a1fe5dbc23693
enejivk <enejivic@gmail.com>
2024-02-15 18:58:24 +0100

    Create user and database

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,9 +5,12 @@
 CREATE DATABASE 
     IF NOT EXISTS hbtn_0d_2;
 
-CREATE USER 'user_0d_2'@'localhost'
+CREATE USER
+    'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 
 GRANT SELECT
-ON hbtn_0d_2.*
-TO 'user_0d_2'@'localhost'
+    ON hbtn_0d_2.*
+    TO 'user_0d_2'@'localhost';
+
+FLUSH PRIVILEGES;
