8bc51c2616eddb979b110043b127112e975d9b97
enejivk <enejivic@gmail.com>
2024-02-15 14:56:22 +0100

    creating a database and a user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,13 @@
+-- Create a database if it doesn't already exist
+-- Grant SELECT privileges on all tables 
+-- in the hbtn_0d_2 database to the user
+
+CREATE DATABASE 
+    IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT
+ON hbtn_0d_2.*
+TO 'user_0d_2'@'localhost'
