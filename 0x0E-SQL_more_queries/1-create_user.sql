fc3e1670c17370be4e4538346852f73362595841
enejivk <enejivic@gmail.com>
2024-02-15 18:36:57 +0100

    Creat a user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,12 +1,13 @@
 -- Creat a user user_0d_1.
 -- If the user does not exist you code should not fail
 
-
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER 
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
 
 GRANT ALL PRIVILEGES
-ON *.*
-TO 'user_0d_1_pwd'@'localhost';
-IDENTIFIED BY 'user_0d_1_pwd';
+    ON *.*
+    TO 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+
 FLUSH PRIVILEGES;
