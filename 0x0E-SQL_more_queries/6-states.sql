05bad224d5bae51e8617103fdc33d5bc8e7873e5
enejivk <enejivic@gmail.com>
2024-02-15 20:16:24 +0100

    Create a database and a table of states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,14 @@
+-- Creating a database hbtn_0d_usa
+-- Creating a table states inside the database
+
+CREATE DATABASE
+    IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE
+    IF NOT EXISTS states (
+    id INTEGER AUTOINCREMENT NOT NULL,
+    name VARCHAR(256),
+    UNIQUE(id)
+    );
