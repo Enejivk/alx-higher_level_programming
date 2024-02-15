a0187d584fc67836eae4da6db7c1118440f6de59
enejivk <enejivic@gmail.com>
2024-02-15 21:20:34 +0100

    Creating a database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,11 +1,14 @@
 -- Creating a database hbtn_0d_usa
 -- Creating a table states inside the database
 
-CREATE database 
+CREATE DATABASE
     IF NOT EXISTS hbtn_0d_usa;
 
 USE hbtn_0d_usa;
-CREATE table I
-    F NOT EXISTS states(
-    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-    name VARCHAR(256) NOT NULL);
+
+CREATE TABLE 
+IF NOT EXISTS states (
+    id INTEGER AUTO_INCREMENT NOT NULL UNIQUE,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
