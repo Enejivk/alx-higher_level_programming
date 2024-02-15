8bab58abd0f50a2208be87580145c24d68bba069
enejivk <enejivic@gmail.com>
2024-02-15 14:27:18 +0100

    Creating a user and granting permission to it

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,10 +1,9 @@
 -- Creat a user user_0d_1.
 -- If the user does not exist you code should not fail
 
-
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES
+GRANT ALL
 ON *.*
-TO 'user_0d_1_pwd';
+TO 'user_0d_1_pwd'@'localhost';
