73394d159defa66300a7a534f0c142eb450cefab
enejivk <enejivic@gmail.com>
2024-02-16 22:41:43 +0100

    Creating database and table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,17 +2,13 @@
 --- It also create a table called cities
 --- Creating database and table should not faile if there already exist
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
+CREATE DATABASE
+    IF NOT EXISTS hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS states (
-    id INTEGER UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS cities (
-    id INTEGER UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+CREATE TABLE
+    IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INTEGER NOT NULL,
     name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states (id)
-);
+    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states (id)
+    )
