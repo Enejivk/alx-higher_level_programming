d469bcf9b56f745b61c00b27220081f40b70f8ec
enejivk <enejivic@gmail.com>
2024-02-16 04:05:54 +0100

    Creating a city table with foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,17 @@
+--- Script that Create a database hbtn_0d_usa
+--- It also create a table called cities
+--- Creating database and table should not faile if there already exist
+
+CREATE DATABASE
+    IF NOT EXISTS hbtn_0d_usa;
+
+USE DATABASE hbtn_0d_usa;
+
+CREATE TABLE
+    IF NOT EXISTS cities (
+    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY UNIQUE,
+    state_id INTEGER NOT NULL,
+    name VARCHAR(256) NOT NULL
+    FOREIGN KEY (state_id) REFERENCES states (id)
+    );
+
