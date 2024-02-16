8311ac6ea8b5cc3739f54349466b70745427629a
enejivk <enejivic@gmail.com>
2024-02-16 19:50:40 +0100

    converting to UTF-8

--- 0x0E-SQL_more_queries/100-move_to_utf8.sql
+++ 0x0E-SQL_more_queries/100-move_to_utf8.sql
@@ -1,5 +1,8 @@
--- Converts the entire database hbtn_0c_0 to UTF8.
+-- Changes a property of a database in the server
 
-USE `hbtn_0c_0`
-ALTER TABLE `first_table`
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0
+    CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Changes the properties of a table in the database
+USE hbtn_0c_0;
+ALTER TABLE first_table
+    CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
