285a41fad2bf04b875d32c8f51ee7886d912e37b
enejivk <enejivic@gmail.com>
2024-03-03 21:10:17 +0100

    show permision

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- This returns the list of the grant for a particular user
+-- In this case there are too users
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
