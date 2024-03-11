81d4b6e2d5bb0058776dc36b959e21ed13141cca
enejivk <enejivic@gmail.com>
2024-03-11 21:53:51 +0100

    second largest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+if (process.argv.length === 2) {
+  console.log(0);
+} else if (process.argv.length === 3) {
+  console.log(1);
+} else {
+  let index = 2;
+  let secondLargest = 0;
+  let largest = parseInt(process.argv[index]);
+  while (index < process.argv.length) {
+    const currentNumber = parseInt(process.argv[index]);
+    if (currentNumber > largest) {
+      secondLargest = largest;
+      largest = currentNumber;
+    }
+    if (currentNumber < largest && currentNumber > secondLargest) {
+      secondLargest = currentNumber;
+    }
+    index++;
+  }
+  console.log(secondLargest);
+}
