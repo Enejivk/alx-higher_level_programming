1c63a56080b8e7aff81c94efdc0eff6635fdbba4
enejivk <enejivic@gmail.com>
2024-03-16 08:04:11 +0100

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,13 +2,13 @@
 """python script that execute Mysql query and return only character
 starting with capital letter N"""
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost',
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
                          port=3306
                          )
     cur = db.cursor()
