6f97716639d4699de8f570d8b26ad64015e2051f
enejivk <enejivic@gmail.com>
2024-03-16 08:52:46 +0100

    print only Arizona

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script that returns a database based on the argument"""
+
+import MySQLdb
+from sys import argv
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost',
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         port=3306
+                         )
+    cur = db.cursor()
+    query = 'SELECT * FROM states WHERE name = "{}"'.format(argv[4])
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
