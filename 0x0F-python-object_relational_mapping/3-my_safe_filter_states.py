6be103ca151410d336f73a7402f9a31ff947e95e
enejivk <enejivic@gmail.com>
2024-03-16 13:10:48 +0100

    checking for special character

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,20 +3,21 @@
 """Adjusting the code to prevent again SQL Injection"""
 
 import MySQLdb
-from sys import argv
+import sys
+
+
 if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost',
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
-                         port=3306
-                         )
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
     cur = db.cursor()    
-    query = """ SELECT * FROM states
+    query = """SELECT * FROM states
             WHERE name LIKE BINARY %s
-            ORDER BY id ASC """
+            ORDER BY id ASC"""
 
-    cur.execute(query, argv[4])
+    cur.execute(query, sys.argv[4])
     rows = cur.fetchall()
     for row in rows:
         print(row)
