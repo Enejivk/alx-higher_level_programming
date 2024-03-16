d7d1e73bd0e7d5a735feb0b86da5afaeb2c803a0
enejivk <enejivic@gmail.com>
2024-03-16 03:32:32 +0100

    selecting all the id and the states name using python

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,22 @@
-#!/usr/bin/python3
-import MySQLdb
 import sys
+import MySQLdb
 
-"""This python connect to the database and list
-content of hbtn_0e_0_usa database"""
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host='localhost',
-                         user=sys.argv[1],
+    db = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3],
-                         port=3306
-                         )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    items = cur.fetchall()
-    for item in items:
-        print(item)
-    cur.close()
+                         host='localhost',
+                         port=3306)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    cursor.close()
     db.close()
