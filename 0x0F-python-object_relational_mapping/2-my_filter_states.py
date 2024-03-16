8c629aca77fc5ede0596ae7d511e6510b19ebc93
enejivk <enejivic@gmail.com>
2024-03-16 09:16:18 +0100

    print only Arizona

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
                          )
     cur = db.cursor()
     query = """ SELECT * FROM states
-            WHERE name LIKE "{}"
+            WHERE name LIKE BINARY "{}"
             ORDER BY id ASC """.format(argv[4])
 
     cur.execute(query)
