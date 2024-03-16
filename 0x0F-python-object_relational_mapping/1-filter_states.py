e27b61ecadbfeab4e1358f24ba8c057246cf7777
enejivk <enejivic@gmail.com>
2024-03-16 07:54:50 +0100

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,8 +14,7 @@ if __name__ == '__main__':
                          port=3306
                          )
     cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id""")
+    cur.execute("SELECT * FROM states WHERE name REGEXP '^N' ORDER BY id ASC")
     data = cur.fetchall()
     for item in data:
         print(item)
