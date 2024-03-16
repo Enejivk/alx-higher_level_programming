f793334941b9f4c097dad5b4649d29989474b88a
enejivk <enejivic@gmail.com>
2024-03-16 09:14:37 +0100

    print only Arizona

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,10 +11,10 @@ if __name__ == '__main__':
                          port=3306
                          )
     cur = db.cursor()
-    query = """ SELECT * FROM states 
-            WHERE name LIKE "{}" 
+    query = """ SELECT * FROM states
+            WHERE name LIKE "{}"
             ORDER BY id ASC """.format(argv[4])
-    
+
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
