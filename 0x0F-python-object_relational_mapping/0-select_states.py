0654a62fc5b8b8baa1f4d3be7a3de20510998d31
enejivk <enejivic@gmail.com>
2024-03-16 03:31:33 +0100

    selecting all the id and the states name using python

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,9 +7,9 @@ content of hbtn_0e_0_usa database"""
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost',
-                         user='secroot',
-                         passwd='secroot',
-                         db='hbtn_0e_0_usa',
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
                          port=3306
                          )
     cur = db.cursor()
