b0f7339016d18c151a03ceac48805cd812fbeb7e
enejivk <enejivic@gmail.com>
2024-03-21 19:03:10 +0100

    printing the first value

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Printing all the state and the Id of the state"""
+
+from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import sessionmaker
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from sys import argv
+
+"""Connecting to the database using engine method"""
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(engine)
+    session = Session()
+    for id, state in session.query(State.id, State.name)\
+                            .order_by(State.id.asc())[:1]:
+        print("{}: {}".format(id, state))
