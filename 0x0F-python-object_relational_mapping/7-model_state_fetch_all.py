949d705af2f8285cb5ef839a7a2e07310b2c2e0e
enejivk <enejivic@gmail.com>
2024-03-21 18:16:30 +0100

    printting all the values in states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,8 +2,8 @@
 """Printing all the state and the Id of the state"""
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import sessionmaker
-# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import sessionmaker
+from sqlalchemy.orm import sessionmaker
 from model_state import State
 from sys import argv
 
@@ -13,6 +13,6 @@ if __name__ == '__main__':
                            .format(argv[1], argv[2], argv[3]))
     Session = sessionmaker(engine)
     session = Session()
-for id, state in session.query(State.id,
-                               State.name).order_by(State.id.asc()):
+for id, state in session.query(State.id,State.name)\
+                        .order_by(State.id.asc()):
     print("{}: {}".format(id, state))
