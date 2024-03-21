0c690a4dbc7e7aaeb04de51bd8b0462f0c5662b4
enejivk <enejivic@gmail.com>
2024-03-21 17:54:02 +0100

    List the content of state table using sqlalchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,16 +2,18 @@
 """Printing all the state and the Id of the state"""
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import sessionmaker
+# from sqlalchemy.ext.declarative import sessionmaker
+from sqlalchemy.orm import sessionmaker
 from model_state import State
 from sys import argv
 
+"""Connecting to the database using engine method"""
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                        .format(argv[1], argv[2], argv[3]))
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                       .format(argv[1], argv[2], argv[3]))
 
-
-Session = sessionmaker(engine)
-session = Session()
-for id, state in session.query(State.id, State.name):
-    print("{}: {}".format(id, state))
+    Session = sessionmaker(engine)
+    session = Session()
+    for id, state in session.query(State.id, State.name).order_by(State.id.asc()):
+        print("{}: {}".format(id, state))
