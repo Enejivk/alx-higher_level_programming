04ab9178e1c2ed33b39e5b8429f87527292f2d0b
enejivk <enejivic@gmail.com>
2024-03-22 12:08:02 +0100

    inner joint

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_city import City
+from model_state import State
+from sys import argv
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(engine)
+    session = Session()
+    result = session.query(State.name, City.id, City.name)\
+                    .join(City, State.id == City.state_id).all()
+    for row in result:
+        print('{}: ({}) {}'.format(row[0], row[1], row[2]))
