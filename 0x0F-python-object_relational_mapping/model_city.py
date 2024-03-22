80f77060d1d6c7758a6a9b95895752c77eb50f61
enejivk <enejivic@gmail.com>
2024-03-22 12:09:00 +0100

    inner joint

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Creating a city table"""
+
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker
+from model_state import Base
+from sys import argv
+
+Base = declarative_base()
+
+class City(Base):
+    """creating a city table"""
+
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('state.id'))
