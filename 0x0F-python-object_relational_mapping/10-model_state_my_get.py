c6ea27babc2599385edccf350567fdcf1a1aee1f
enejivk <enejivic@gmail.com>
2024-03-22 01:45:02 +0100

    return a particular state based on an argument

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""List all the state that contain a"""
+
+from sqlalchemy import create_engine
+from model_state import State, Base
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sys import argv
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    Session = sessionmaker(engine)
+    session = Session()
+    search_string = argv[4]
+    part = session.query(State).filter(State.name.like(search_string)).all()
+    if part:
+        for value in part:
+            print('{}: {}'.format(value.id, value.name))
+    else:
+        print("Not found")
