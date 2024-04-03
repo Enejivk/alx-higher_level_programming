8e7bf5ac999bfc31c25a67b9edc1db57dfbbb98e
enejivk <enejivic@gmail.com>
2024-04-03 20:38:04 +0100

    content_length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that return the exact number of the content length
-curl -Is "$1" | grep -w 'Content-length' | cut -f2 -d' '
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
