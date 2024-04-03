16ef5149a2a42e41d78b91c9b903c4b8b336dc45
enejivk <enejivic@gmail.com>
2024-04-03 20:33:32 +0100

    content_length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that return
-curl -Is "$1" | grep -w 'content-length' | cut -f2 -d' '
+# script to get the body size of a request
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
