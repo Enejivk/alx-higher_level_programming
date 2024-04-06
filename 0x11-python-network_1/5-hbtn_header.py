d69506389c3c11c9bd52ae893a9bdf71a0b4e114
enejivk <enejivic@gmail.com>
2024-04-06 18:39:13 +0100

    printing a x-value in the head

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Getting the value of X-Request-Id from the header
+"""
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+    url = argv[1]
+    r = requests.get(url)
+    header = r.headers
+    print(header['X-Request-Id'])
