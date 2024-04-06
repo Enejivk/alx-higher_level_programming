d8d721721d7a8cd9fa3613046cc3983056a2675a
enejivk <enejivic@gmail.com>
2024-04-06 10:56:57 +0100

    printing the infor of a url response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,12 @@
 """
 Printing the body of url response
 """
-import urllib.request
+if __name__ == "__main__":
+    import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    body = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(body)))
-    print('\t- content: {}'.format(body))
-    print('\t- utf8 content: {}'.format(body.decode('utf-8')))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(body)))
+        print('\t- content: {}'.format(body))
+        print('\t- utf8 content: {}'.format(body.decode('utf-8')))
