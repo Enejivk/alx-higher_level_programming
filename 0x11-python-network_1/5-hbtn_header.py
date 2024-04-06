9b5186362b99fa24249b801fb2f1342731cd16c4
enejivk <enejivic@gmail.com>
2024-04-06 18:45:22 +0100

    getting x value in header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,7 +5,8 @@ Getting the value of X-Request-Id from the header
 if __name__ == "__main__":
     from sys import argv
     import requests
+
     url = argv[1]
     r = requests.get(url)
     header = r.headers
-    print(header['X-Request-Id'])
+    print(header.get['X-Request-Id'])
