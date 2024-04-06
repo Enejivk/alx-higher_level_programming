c6e72438e534387427743b5fbaa5b2a276698b75
enejivk <enejivic@gmail.com>
2024-04-06 19:32:12 +0100

    checking for status code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,15 +2,13 @@
 """
 Checking an error that is greater than 400
 """
-
 from sys import argv
 import requests
 
 if __name__ == "__main__":
     url = argv[2]
-    r = requests(url)
+    r = requests.get(url)
     statusCode = r.status_code
-
     if statusCode >= 400:
         print("Error code: {}".format(statusCode))
     else:
