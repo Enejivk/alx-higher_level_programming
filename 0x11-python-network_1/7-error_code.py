7c2da1d7e95bac797bc128e6574a3e24c1ea3182
enejivk <enejivic@gmail.com>
2024-04-06 19:36:06 +0100

    checking for status code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ from sys import argv
 import requests
 
 if __name__ == "__main__":
-    url = argv[2]
+    url = argv[1]
     r = requests.get(url)
     statusCode = r.status_code
     if statusCode >= 400:
