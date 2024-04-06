e3ee25587be88e7d1fa8cf8e09abacba13f3ef2c
enejivk <enejivic@gmail.com>
2024-04-06 10:41:05 +0100

    X-Request-Id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,8 +2,9 @@
 """
 printing the value of X-Request-Id
 """
-import urllib.request
-from sys import argv
-with urllib.request.urlopen(argv[1]) as response:
-    header = response.info()
-    print(header['X-Request-Id'])
+if __name__ == "__main__":
+    import urllib.request
+    from sys import argv
+    with urllib.request.urlopen(argv[1]) as response:
+        header = response.info()
+        print("{}".format(header.get("X-Request-Id")))
