ad1451a8c3b93831178fa5367b89906dcd6adc58
enejivk <enejivic@gmail.com>
2024-04-06 12:14:51 +0100

    geting the status code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+checking for http error
+"""
+if __name__ == "__main__":
+    import urllib.request
+    from sys import argv
+    from urllib.error import HTTPError
+    url = argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode()
+    except HTTPError as e:    
+        print('Error code: {}'.format(e.code))     
