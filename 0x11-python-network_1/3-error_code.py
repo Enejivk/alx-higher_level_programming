d52f7c5d01fce64939c1cd01af888baedee5f827
enejivk <enejivic@gmail.com>
2024-04-06 12:33:47 +0100

    geting the status code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
 """
-checking for http error
-"""
+import sys
+import urllib.error
+import urllib.request
+
+
 if __name__ == "__main__":
-    import urllib.request
-    from sys import argv
-    from urllib.error import HTTPError
-    url = argv[1]
-    
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read().decode('ascii')
-            print(body)
-    except HTTPError as e:    
-        print('Error code: {}'.format(e.code))
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
