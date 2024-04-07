ea5def3caf96294cd3542cab42df75c3087f445c
enejivk <enejivic@gmail.com>
2024-04-07 15:36:51 +0100

    github id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Python script that get github ID from github API
+"""
+
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+if __name__ == "__main__":
+    user_name = argv[1]
+    password = argv[2]
+    basic = HTTPBasicAuth(user_name, password)
+    url = "https://api.github.com/users/{}".format(user_name)
+    request = requests.get(url, auth=basic)
+    print(request.json().get('id'))
+    print(request.json())
