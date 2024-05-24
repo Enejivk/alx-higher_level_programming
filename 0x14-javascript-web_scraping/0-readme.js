2a421fc44a82b35137244692a663547499e8f5bc
enejivk <enejivic@gmail.com>
2024-05-24 08:06:59 +0100

    correcting indentation

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-
-const path = process.argv[2];
 const fs = require('fs');
 
+const path = process.argv[2];
 fs.readFile(path, 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
+  if (err) throw err;
+  console.log(data);
 });
