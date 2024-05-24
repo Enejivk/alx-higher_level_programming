8a9a01487869f5ce459456170ac55d139a2f0b20
enejivk <enejivic@gmail.com>
2024-05-24 16:37:18 +0100

    gettiing the standard code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,10 @@
 
 const request = require('request');
 const url = process.argv[2];
-request(url, (error, response, body) => {
-  if (error) throw error;
-  console.log(response.statusCode);
+request(url, (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(response.statusCode);
+  }
 });
