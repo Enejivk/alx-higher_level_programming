8c9acf11dac590a9a59b920e684bd5becfbc5a05
enejivk <enejivic@gmail.com>
2024-05-25 12:26:40 +0100

    handling of errors

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,28 +1,26 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const url = process.argv[2];
-request(url, (error, response, body) => {
-  if (error) throw error;
-  const json = JSON.parse(body);
-  countTask(json);
-});
 
-const countTask = (content) => {
-  const obj = {};
-  for (let i = 1; i <= 10; i++) {
-    let count = 0;
-    for (const task of content) {
-      if (task.userId === i && task.completed === true) {
-        count += 1;
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
       }
-      obj[i] = count;
     }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
-  printing(obj);
-};
-
-function printing (obj) {
-  console.log(obj);
-}
+});
